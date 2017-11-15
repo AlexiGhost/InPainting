655adbdf1422f852f55639b5325051574bed8117
Nebulord <matthieucabanes04@gmail.com>
2017-11-15 16:14:06 +0100

    patch

--- src/topology/Patch.java
+++ src/topology/Patch.java
@@ -4,14 +4,14 @@ public class Patch{
 	private Point point;	
 	private BoundingBox boundingBox;
 	
-	public Patch(Point _point,int halfwidth,BoundingBox window){		
+	public Patch(Point _point, int halfwidth, BoundingBox window) {		
 	point = _point;
-	boundingBox = new BoundingBox(new int[]{
-		Math.max(0, point.getI() - halfwidth) - point.getI(),
-		Math.max(0, point.getJ() - halfwidth) - point.getJ(),
-		Math.min(window.getWidth(), point.getJ() + halfwidth) - point.getI(),
-		Math.min(window.getHeight(), point.getJ() + halfwidth) - point.getJ()
-		});
+	int[] tab = new int[4];
+	tab[0] = Math.max(0, point.getI() - halfwidth) - point.getI();
+	tab[1] = Math.max(0, point.getJ() - halfwidth) - point.getJ();
+	tab[2] = Math.min(window.getWidth(), point.getJ() + halfwidth) - point.getI();
+	tab[3] = Math.min(window.getHeight(), point.getJ() + halfwidth) - point.getJ();
+	boundingBox = new BoundingBox(tab);	
 	}
 	
 	public Point getPoint() {
