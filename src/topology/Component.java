2081a86f3ffe8b6ce53cc018955b491b7c02bf3b
MYRIAM\Admin <meriamzek@hotmail.com>
2017-12-13 17:17:05 +0100

    Component squelette

---
+++ src/topology/Component.java
@@ -0,0 +1,15 @@
+package topology;
+
+import java.util.List;
+
+public class Component {
+	private List<Point> points;
+	
+	public Component(Tag tag, Point seedPoint) {
+		
+	}
+	
+	public String toString(){
+		return "";
+	}
+}
