af94aaf6b65763f487014e8a17f5b4d03d7fccbb
Courieux Alexi <courieuxa@gmail.com>
2017-12-13 17:18:28 +0100

    Components

---
+++ src/topology/Components.java
@@ -0,0 +1,15 @@
+package topology;
+
+import java.awt.Component;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Components {
+	public List<Component> components = new ArrayList<>();
+	public int size(){
+		return components.size();
+	}
+	public Components(Boundary boundary){
+		
+	}
+}
