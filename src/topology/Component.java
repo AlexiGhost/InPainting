0995b8de4af39429b9052713c332cffebcee7068
MYRIAM\Admin <meriamzek@hotmail.com>
2017-12-13 22:14:54 +0100

    Component

--- src/topology/Component.java
+++ src/topology/Component.java
@@ -6,10 +6,40 @@ public class Component {
 	private List<Point> points;
 	
 	public Component(Tag tag, Point seedPoint) {
+		Point point = seedPoint;
+		int index;
 		
+		index = tag.indexActiveOuterEdge(point);
+		Edge edge = null;
+		Point[] edgePoints = null; 
+		
+		while(index != -1){
+			points.add(point);
+			edge = tag.boundary.getEdges().get(index);
+			edgePoints = edge.border();
+			
+			if(edgePoints[0].getI() != point.getI() || edgePoints[0].getJ() != point.getJ())
+				point = edgePoints[0];
+			else
+				point = edgePoints[1];
+			
+			tag.active[index] = false;
+			index = tag.indexActiveOuterEdge(point);
+		}
+		
+		points.add(point);
 	}
 	
 	public String toString(){
-		return "";
+		String s = "Liste des points :\n";
+		int i = 1;
+		
+		for (Point point : points) {
+			s += i+"-\ti: "+point.getI()+"; j: "+point.getJ();
+		}
+		
+		s += "\n----------------------------------------------------------------\n";
+		
+		return s;
 	}
 }
