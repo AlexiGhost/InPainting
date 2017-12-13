04f031eadacab8e62ee5148b920856aa17001034
Courieux Alexi <courieuxa@gmail.com>
2017-12-13 17:30:57 +0100

    fin du cours

--- src/topology/Components.java
+++ src/topology/Components.java
@@ -1,14 +1,16 @@
 package topology;
 
-import java.awt.Component;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Components {
+	/**Array of all the connected components of the boundary of the mask*/
 	public List<Component> components = new ArrayList<>();
+	/**Return the number of connected components*/
 	public int size(){
 		return components.size();
 	}
+	/**Construct the list of all connected components of the given mask*/
 	public Components(Boundary boundary){
 		
 	}
