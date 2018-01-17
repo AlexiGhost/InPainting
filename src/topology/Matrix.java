f09d334425cb4471d678c72b4e63164ae6dbfd82
Courieux Alexi <courieuxa@gmail.com>
2018-01-17 16:29:24 +0100

    clear unused imports

--- src/topology/Matrix.java
+++ src/topology/Matrix.java
@@ -3,7 +3,6 @@ package topology;
 /** @author Matthieu Cabanes
  * @author Meriam Zekri*/
 
-import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
